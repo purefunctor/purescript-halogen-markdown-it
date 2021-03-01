46658283eebb523d9713d715d2e39f4985f9013f
PureFunctor <purefunctor@gmail.com>
2021-03-01 13:18:17 +0800

    Add partial support for headings and paragraphs

---
+++ src/Halogen/Markdown.purs
@@ -0,0 +1,68 @@
+module Halogen.Markdown where
+
+import Prelude
+
+import Control.Alternative ((<|>))
+import Data.Either (Either)
+import Data.List (List)
+import Data.List as List
+import Data.String as String
+import Text.Parsing.StringParser (ParseError, Parser, fail, runParser, try)
+import Text.Parsing.StringParser.CodePoints (char, eof, regex, skipSpaces, whiteSpace)
+import Text.Parsing.StringParser.Combinators (choice, many)
+
+
+data MarkdownAST
+  = Heading Level String
+  | Text String
+  | BlankLine
+  | Block (List MarkdownAST)
+
+derive instance eqMarkdownAST :: Eq MarkdownAST
+
+instance showMarkdownAST :: Show MarkdownAST where
+  show = case _ of
+    Heading level text -> List.intercalate " " [ "Heading", show level, text ]
+    Text text -> List.intercalate " " [ "Text", text ]
+    BlankLine -> "BlankLine"
+    Block text -> List.intercalate " " [ "Block", show text ]
+
+
+newtype Level = Level Int
+
+derive instance eqLevel :: Eq Level
+derive instance ordLevel :: Ord Level
+
+instance showLevel :: Show Level where
+  show (Level level) = show level
+
+
+pHeader :: Parser MarkdownAST
+pHeader = do
+  leading <- String.length <$> whiteSpace
+
+  when (leading > 3) (fail "Invalid heading")
+
+  level <- String.length <$> regex "#+"
+  skipSpaces
+  title <- regex ".*" <* (many $ char '\n')
+
+  when (level > 6) (fail "Invalid heading")
+
+  pure $ Heading (Level level) title
+
+
+pBlank :: Parser MarkdownAST
+pBlank = char '\n' *> pure BlankLine
+
+
+pText :: Parser MarkdownAST
+pText = Text <$> (skipSpaces *> regex ".+" <* ((void $ char '\n') <|> eof))
+
+
+pBlock :: Parser MarkdownAST
+pBlock = Block <$> (many $ choice $ try <$> [pHeader, pBlank, pText])
+
+
+parseMarkdown :: String -> Either ParseError MarkdownAST
+parseMarkdown = runParser pBlock
