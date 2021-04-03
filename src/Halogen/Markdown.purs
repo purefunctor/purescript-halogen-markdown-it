7bf35428c7982869e68e4554d52a260e4a780ff0
PureFunctor <purefunctor@gmail.com>
2021-04-03 16:25:23 +0800

    Add missing transformation steps

--- src/Halogen/Markdown.purs
+++ src/Halogen/Markdown.purs
@@ -2,16 +2,19 @@ module Halogen.Markdown where
 
 import Prelude
 
+import Data.Array as Array
 import Data.Either (hush)
 import Data.Maybe (Maybe)
 import Halogen.HTML as HH
 import Halogen.Markdown.Parser (lines)
-import Halogen.Markdown.Transfomer (toHalogen)
+import Halogen.Markdown.Transfomer (normalize, toHalogen)
 import Text.Parsing.StringParser (runParser)
 
 {-----------------------------------------------------------------------}
 
 parseMarkdown :: forall w a. String -> Maybe ( Array ( HH.HTML w a ) )
-parseMarkdown = map toHalogen <<< hush <<< runParser lines
+parseMarkdown = transform <=< hush <<< runParser lines
+  where
+    transform = pure <<< Array.reverse <<< toHalogen <<< normalize
 
 {-----------------------------------------------------------------------}
