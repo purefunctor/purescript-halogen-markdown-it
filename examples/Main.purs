1f9bcd33df643de006239a0e6cb23e08d39b6743
PureFunctor <purefunctor@gmail.com>
2021-04-22 09:43:39 +0800

    Create initial example

--- examples/Main.purs
+++ examples/Main.purs
@@ -2,7 +2,62 @@ module Main where
 
 import Prelude
 
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
+import Effect.Aff (Aff)
+import Foreign.Object as Object
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
+import Halogen.MarkdownIt as HM
+import Halogen.Storybook (Stories, proxy, runStorybook)
 
 
 foreign import highlightBlocks :: Effect Unit
+
+
+stories :: Stories Aff
+stories = Object.fromFoldable
+  [ Tuple "Halogen Markdown" $ proxy home ]
+  where
+    home =
+      H.mkComponent
+        { initialState: \_ -> HH.div [ ] [ HH.text "Loading..." ]
+        , eval: H.mkEval $ H.defaultEval
+          { handleAction = handleAction
+          , initialize = Just unit
+          }
+        , render
+        }
+      where
+        render = identity
+
+    handleAction _ = do
+      H.liftEffect $ highlightBlocks
+
+      html_ <- H.liftEffect $
+        HM.viewFromMd markdown HH.article_
+
+      H.put $ case html_ of
+        Left e -> HH.h1_ [ HH.text e ]
+        Right h -> h
+
+
+css :: forall r a. String -> HP.IProp ( class :: String | r ) a
+css = HP.class_ <<< H.ClassName
+
+
+markdown :: String
+markdown = """
+# Hello, World
+This is a paragraph for example.
+"""
+
+
+main :: Effect Unit
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runStorybook { stories, logo: Just $ HH.text "Halogen Markdown" } body
--- spago.dhall
+++ spago.dhall
@@ -1,24 +1,20 @@
 { name = "halogen-markdown-it"
 , dependencies =
   [ "arrays"
+  , "bifunctors"
   , "console"
-  , "control"
-  , "dom-indexed"
   , "effect"
   , "either"
   , "halogen"
   , "lists"
   , "maybe"
-  , "newtype"
   , "prelude"
   , "psci-support"
   , "simple-markdown-it"
   , "strict-html-parser"
   , "string-parsers"
-  , "strings"
   , "tailrec"
   , "tuples"
-  , "web-events"
   ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
